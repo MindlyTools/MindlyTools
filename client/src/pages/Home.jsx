e9f2970c4a47e133592582629d455630fc20c40b
“mamta” <mamtadhone9@gmail.com>
2025-11-30 19:39:55 +0000

    Implemented calculator tool with styling, back button with routing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,75 +12,79 @@ export default function Home({ user }) {
 
   return (
     <>
-    <Background>
-      <Header />
-      <div style={{ display: "flex" }}>
-        {/* Main Content */}
-        <div
-          style={{
-            flex: 1,
-            transition: "margin-left 0.3s ease",
-          }}
-        >
-          <div className="mindly-heading">
-            <span className="mindly">
-              <strong>Mindly</strong>
-            </span>
-            <span className="tools">
-              <strong>Tools</strong>
-            </span>
-          </div>
+      <Background>
+        <Header />
+        <div style={{ display: "flex" }}>
+          {/* Main Content */}
+          <div
+            style={{
+              flex: 1,
+              transition: "margin-left 0.3s ease",
+            }}
+          >
+            <div className="mindly-heading">
+              <span className="mindly">
+                <strong>Mindly</strong>
+              </span>
+              <span className="tools">
+                <strong>Tools</strong>
+              </span>
+            </div>
 
-          <div className="tool-container">
-            <TiltedCard
-              displayOverlayContent={true}
-              overlayContent={
-                <ToolCard
-                  title="Todo List"
-                  description="Simple tool to manage tasks"
-                />
-              }
-            />
-            <TiltedCard
-              displayOverlayContent={true}
-              overlayContent={
-                <ToolCard title="Tool 2" description="Description" />
-              }
-            />
-            <TiltedCard
-              displayOverlayContent={true}
-              overlayContent={
-                <ToolCard title="Tool 3" description="Description" />
-              }
-            />
-            <TiltedCard
-              displayOverlayContent={true}
-              overlayContent={
-                <ToolCard title="Tool 4" description="Description" />
-              }
-            />
-            <TiltedCard
-              displayOverlayContent={true}
-              overlayContent={
-                <ToolCard title="Tool 5" description="Description" />
-              }
-            />
-            <TiltedCard
-              displayOverlayContent={true}
-              overlayContent={
-                <ToolCard title="Tool 6" description="Description" />
-              }
-            />
+            <div className="tool-container">
+              <TiltedCard
+                displayOverlayContent={true}
+                overlayContent={
+                  <ToolCard
+                    title="Todo List"
+                    description="Simple tool to manage tasks"
+                  />
+                }
+              />
+              <TiltedCard
+                displayOverlayContent={true}
+                overlayContent={
+                  <ToolCard
+                    title="Calculator"
+                    description="Simple calculator for performing accurate operations"
+                    route="/calculator"
+                  />
+                }
+              />
+              <TiltedCard
+                displayOverlayContent={true}
+                overlayContent={
+                  <ToolCard title="Tool 3" description="Description" />
+                }
+              />
+              <TiltedCard
+                displayOverlayContent={true}
+                overlayContent={
+                  <ToolCard title="Tool 4" description="Description" />
+                }
+              />
+              <TiltedCard
+                displayOverlayContent={true}
+                overlayContent={
+                  <ToolCard title="Tool 5" description="Description" />
+                }
+              />
+              <TiltedCard
+                displayOverlayContent={true}
+                overlayContent={
+                  <ToolCard title="Tool 6" description="Description" />
+                }
+              />
+            </div>
           </div>
+          <Sidebar
+            user={user}
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+          />
         </div>
-        <Sidebar
-          user={user}
-          sidebarOpen={sidebarOpen}
-          setSidebarOpen={setSidebarOpen}
-        />
-      </div>
-      <Footer />
-    </Background>
+        <Footer />
+      </Background>
     </>
   );
 }
