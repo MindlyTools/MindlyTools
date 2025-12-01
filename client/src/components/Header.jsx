c0a0e8db6d25b178aa92e3aad5fa2f842301931f
DavidAdam1323 <davidadamgtr@gmail.com>
2025-12-01 00:45:09 +0000

    Integrated hamburger button for sidebar toggle functionality and improve header layout

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,22 @@
-import Logo from "../components/Logo"
+import Logo from "../components/Logo";
+import HamburgerButton from "../components/HamburgerButton";
 import "../styles/header.css";
 
-export default function Header() {
+export default function Header({ sidebarOpen, setSidebarOpen }) {
   return (
     <header className="site-header">
       <div className="header-container">
-        <a href="/" className="logo">
-          <Logo />
-        </a>
-        
+        <div className="inner-container">
+          <a href="/" className="logo">
+            <Logo />
+          </a>
+
+          <HamburgerButton
+          isOpen={sidebarOpen}
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+          className=""
+          />
+        </div>
         <nav className="main-nav">
           <ul className="nav-list">
             <li>
