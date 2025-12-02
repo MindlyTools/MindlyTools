649506a22a563a0cb24254e01a0896032dd699b1
Hamza <hamza.nasir2@gmail.com>
2025-12-02 19:54:00 +0000

    updated firebase.js to take in new .env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,6 +5,8 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL, 
 };
 
 const app = initializeApp(firebaseConfig);
